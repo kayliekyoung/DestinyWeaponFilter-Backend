d1f3b694047003598969c33218232551e6242546
DKYoungOne <bawsinator@gmail.com>
2025-02-24 18:20:03 -0600

    nothing to see here

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-const port = process.env.PORT||3006;
+const port = process.env.PORT||3306;
 
 app.get("/", (req, res) => {
     res.json("Server test");
